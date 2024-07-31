e47852d70c36d6386ddad1604af59275f7412c21
complex <rianlotter@icloud.com>
2024-07-31 18:17:28 +0200

    changes(overall): Moved Utils to Shared / Commands use Exports

--- client/client.lua
+++ client/client.lua
@@ -32,34 +32,23 @@ exports('ChangeText', changeText)
 if Config.debugMode then
     -- Command to initialize the Text UI
     RegisterCommand('showTextUI', function(source, args, rawCommand)
-        -- Example arguments
-        local text = "Open Garage"
-        local keybind = "E"
-        local position = "top-middle"
-
-        -- Call the function to initialize the Text UI
-        showTextUI(text, keybind, position)
+        exports.complex_textui:showTextUI("Open Garage", "F1", "bottom-middle")
     end, false)
 
     -- Command to hide the Text UI
     RegisterCommand('hideTextUI', function(source, args, rawCommand)
-        -- Call the function to hide the Text UI
-        hideTextUI()
+        exports.complex_textui:hideTextUI()
     end, false)
 
     -- Command to change the Text UI text
     RegisterCommand('changeText', function(source, args, rawCommand)
-        -- Get the new text from the command arguments
         local newText = table.concat(args, " ")
-        -- Call the function to change the text in the Text UI
-        changeText(newText)
+        exports.complex_textui:changeText(newText)
     end, false)
 
     -- Command to change the Text UI keybind
     RegisterCommand('changeKeybind', function(source, args, rawCommand)
-        -- Get the new keybind from the command arguments
-        local newKeybind = args[1] or "E" -- Default to "E" if no keybind is provided
-        -- Call the function to change the keybind in the Text UI
-        changeKeybind(newKeybind)
+        local newKeybind = args[1] or "E"
+        exports.complex_textui:changeKeybind(newKeybind)
     end, false)
 end
